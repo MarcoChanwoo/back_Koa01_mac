ef8a53ce77a231cefeda892b7eb48a757da83050
Marco <cwhong8087@gmail.com>
2023-09-07 15:57:31 +0900

    5th commit

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -5,8 +5,9 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.patch('/:id', postsCtrl.update);
+
+posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read); // ObjectId 검증이 필요한 부분에 미들웨어 추가
+posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
 
 export default posts;

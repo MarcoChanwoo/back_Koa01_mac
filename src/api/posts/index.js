ed8b674a26f3e2c351a0a925146e9292361ef9af
Marco <cwhong8087@gmail.com>
2023-09-07 21:05:49 +0900

    final commit

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,13 +1,17 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
-posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read); // ObjectId 검증이 필요한 부분에 미들웨어 추가
-posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
-posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+const post = new Router();
+post.get('/', postsCtrl.read); // ObjectId 검증이 필요한 부분에 미들웨어 추가
+post.delete('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch('/', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
+
+posts.use('/:id', postsCtrl.getPostById, post.routes());
 
 export default posts;

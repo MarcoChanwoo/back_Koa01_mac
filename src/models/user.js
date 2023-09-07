bc5583e0174e41ddd4b3fc6b33aeb27552f1e73f
Marco <cwhong8087@gmail.com>
2023-09-07 19:52:52 +0900

    9th commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 import bcrypt from 'bcrypt';
+import Jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema({
     username: String,
@@ -22,6 +23,21 @@ UserSchema.methods.serialize = function () {
     const data = this.toJSON();
     delete data.hashedPassword;
     return data;
+};
+
+UserSchema.methods.generateToken = function () {
+    const token = Jwt.sign(
+        // 첫번째 파라미터에는 토큰 안에 집어넣고 싶은 데이터를 넣음
+        {
+            _id: this.id,
+            username: this.username,
+        },
+        process.env.JWT_SECRET, // 두 번째 파라미터에는 JWT 암호를 넣음
+        {
+            expiresIn: '7d', // 7일간 유효함
+        },
+    );
+    return token;
 };
 
 UserSchema.statics.findByUsername = function (username) {
